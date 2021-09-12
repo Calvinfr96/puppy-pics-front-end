535aa9f13fbf40d94964213948dc5050b4e11691
calvin <calvin.francis96@outlook.com>
2021-09-11 22:01:42 -0400

    Add Nav Bar

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import '../App.css';
 import DogsContainer from './DogsContainer';
 import Header from './Header';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Nav from './Nav';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Nav />
         <Header />
         <Switch>
           <Route path="/" exact component={DogsContainer} />
---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Nav() {
+    return (
+        <nav>
+          <ul className="nav-links">
+            <li>Home</li>
+            <li>New User</li>
+            <li>New Dog</li>
+            <li>New Breed</li>
+          </ul>  
+        </nav>
+    )
+}
+
+export default Nav
