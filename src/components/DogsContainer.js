21d16c74bac2737c742aa022dc35663c6337a7b5
calvin <calvin.francis96@outlook.com>
2021-09-11 20:53:34 -0400

    Refactor fetch call using async/await

--- src/components/DogsContainer.js
+++ src/components/DogsContainer.js
@@ -5,10 +5,14 @@ function DogsContainer() {
     const baseURL = "https://desolate-waters-34836.herokuapp.com"
     const [dogs, setDogs] = useState([])
     useEffect(() => {
-        fetch(`${baseURL}/dogs`)
-            .then(resp => resp.json())
-            .then(data => setDogs(data))
+        fetchAllDogs()
     }, [baseURL])
+
+    const fetchAllDogs = async () => {
+        const data = await fetch(`${baseURL}/dogs`)
+        const dogs = await data.json()
+        setDogs(dogs)
+    }
 
     const dogComponents = dogs.map(dog => {
         return <Dog key={dog.id} dog={dog} />
