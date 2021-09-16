2d2b9ce923604a7025c3f4ab91a8d9edade375a2
calvin <calvin.francis96@outlook.com>
2021-09-16 18:57:43 -0400

    Remove unused useEffect hook from user and breed form

--- src/components/NewBreedForm.js
+++ src/components/NewBreedForm.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 
 function NewBreedForm() {
     const baseURL = "https://desolate-waters-34836.herokuapp.com"
